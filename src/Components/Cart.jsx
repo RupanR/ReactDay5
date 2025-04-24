fad3774731b216644d9180ae0e609b6fc25d6e42
RupanR <rupanmentor@gmail.com>
2025-04-24 18:34:56 +0530

    updated

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,23 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Cart = () => {
-    const [item,setItem] = useState(0)
+  const [item, setItem] = useState(0);
+  const [status, setStatus] = useState(true);
 
-const handleDecrement = () => {
-    if(item>0){
-        setItem(val=>val-1)
+  const handleDecrement = () => {
+    setStatus(true);
+    if (item > 0) {
+      setItem((val) => val - 1);
     }
-   
-}
+  };
 
-    return (
-        <div>
-            <h1>Cart Component</h1>
-            <h1>Cart Value: {item}</h1>
-            <button onClick={()=>{setItem(val=>val+1)}}>Add To Cart</button>
-            <button onClick={handleDecrement}>Remove From Cart</button>
-        </div>
-    );
+  const handleIncrement = () => {
+    setStatus(false);
+    setItem((val) => val + 1);
+  };
+
+  return (
+    <div>
+      <h1>Cart Component</h1>
+      <h1>Cart Value: {item}</h1>
+      {status ? (
+        <button onClick={handleIncrement}>Add To Cart</button>
+      ) : (
+        <button onClick={handleDecrement}>Remove From Cart</button>
+      )}
+    </div>
+  );
 };
 
 export default Cart;
